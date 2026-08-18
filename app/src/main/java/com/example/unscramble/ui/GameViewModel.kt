e0ddb6f472510a015af1e65bb2734fc2097f2b31
Himanshu <marvelstudiom64@gmail.com>
2026-08-18 18:03:26 +0530

    added update score and wordcount logic

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -9,6 +9,7 @@ import com.example.unscramble.data.allWords
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.unscramble.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.update
 
 class GameViewModel:ViewModel() {
@@ -51,14 +52,33 @@ class GameViewModel:ViewModel() {
         userGuess = guessedWord
     }
     fun checkUserGuess() {
-
         if (userGuess.equals(currentWord, ignoreCase = true)) {
+            //user case is correct,increase the score
+            // and call updateGameState() to prepare for next round
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
         } else {
+            //user guess is wrong,show an error
             _uiState.update { currentState ->
                 currentState.copy(isGuessedWordWrong=true)
             }
         }
         // Reset user guess
+        updateUserGuess("")
+    }
+    private fun updateGameState(updateScore: Int){
+        _uiState.update{currentState->
+            currentState.copy(
+                isGuessedWordWrong = false,
+                currentScrambleWord = pickRandomWordAndShuffle(),
+                score = updateScore,
+                currentWordCount = currentState.currentWordCount.inc()
+            )
+        }
+    }
+    fun skipWord(){
+        updateGameState(_uiState.value.score)
+        //reset user guess
         updateUserGuess("")
     }
 }
